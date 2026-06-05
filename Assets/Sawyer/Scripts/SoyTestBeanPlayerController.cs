95a07571e8d60ba6e664c89adbdc546bc9399fd0
COMPUTERSAURUS\meinc <sawyer.james.like@gmail.com>
2026-06-05 12:06:45 -0700

    Modified Player Controller >:) Added foundation structure placing

--- Assets/Sawyer/Scripts/SoyTestBeanPlayerController.cs
+++ Assets/Sawyer/Scripts/SoyTestBeanPlayerController.cs
@@ -3,14 +3,7 @@ using UnityEngine.InputSystem;
 
 public class SoyTestBeanPlayerController : MonoBehaviour
 {
-    private Rigidbody rb;
-    [SerializeField] float speed;
-    private float cammyPitch;
-    [SerializeField] float camSensitivity;
-    [SerializeField] GameObject cameraHH;
-    [SerializeField] float jumpVelocity;
-    private float lastJumpTime;
-    [SerializeField] float jumpCooldown;
+    [SerializeField] private Camera cam;
     [SerializeField] GameObject foundationPrefab;
     private bool isPreviewing = false;
     private GameObject previewStructure;
@@ -19,26 +12,12 @@ public class SoyTestBeanPlayerController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
-        Cursor.lockState = CursorLockMode.Locked;
+        //cam = GetComponent<Camera>();
     }
 
     // Update is called once per frame
     void Update()
     {
-       
-        /*rb.linearVelocity = transform.rotation * new Vector3(Input.GetAxis("Horizontal") * speed, rb.linearVelocity.y, Input.GetAxis("Vertical") * speed);
-        rb.linearVelocity = transform.rotation * new Vector3(((Keyboard.current.rightArrowKey.IsPressed() ? 1 : 0) - (Keyboard.current.leftArrowKey.IsPressed() ? 1 : 0)) * speed, rb.linearVelocity.y, ((Keyboard.current.upArrowKey.IsPressed() ? 1 : 0) - (Keyboard.current.downArrowKey.IsPressed() ? 1 : 0)) * speed);
-        transform.Rotate(0, ((Keyboard.current.dKey.IsPressed() ? 1 : 0) - (Keyboard.current.aKey.IsPressed() ? 1 : 0)) * camSensitivity, 0);
-        cammyPitch = Mathf.Clamp(cammyPitch - ((Keyboard.current.wKey.IsPressed() ? 1 : 0) - (Keyboard.current.sKey.IsPressed() ? 1 : 0)) * camSensitivity, -90, 90);
-        cameraHH.transform.localRotation = Quaternion.Euler(cammyPitch, 0, 0);
-
-        if (Keyboard.current.spaceKey.IsPressed() && (Time.time > lastJumpTime + jumpCooldown))
-        {
-            rb.linearVelocity = new Vector3(rb.linearVelocity.x, rb.linearVelocity.y + jumpVelocity, rb.linearVelocity.z);
-           lastJumpTime = Time.time;
-        }
-        */
         if (Mouse.current.rightButton.wasPressedThisFrame)
         {
             Destroy(previewStructure);
@@ -67,7 +46,7 @@ public class SoyTestBeanPlayerController : MonoBehaviour
         if (isPreviewing)
         {
             RaycastHit hit;
-            if (Physics.Raycast(transform.position, cameraHH.transform.TransformDirection(Vector3.forward), out hit, 15, LayerMask.GetMask("Structure")))
+            if (Physics.Raycast(transform.position, cam.transform.TransformDirection(Vector3.forward), out hit, 15, LayerMask.GetMask("Structure")))
             {
                 StructureScript.SnapPoint[] snapsToCheck = hit.collider.gameObject.GetComponent<StructureScript>().snapPoints;
 
@@ -101,7 +80,7 @@ public class SoyTestBeanPlayerController : MonoBehaviour
                     
             }
 
-            else if (Physics.Raycast(transform.position, cameraHH.transform.TransformDirection(Vector3.forward), out hit, 15))
+            else if (Physics.Raycast(transform.position, cam.transform.TransformDirection(Vector3.forward), out hit, 15))
             {
                 previewStructure.transform.position = hit.point;
                 previewStructure.transform.rotation = transform.rotation;
